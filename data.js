441bc41f942dab3d4b0aff93234dc841ee7ae91c
Barrett Anderson <barrettrees@gmail.com>
2021-03-31 20:09:10 -0700

    first commit

---
+++ data.js
@@ -0,0 +1,210 @@
+
+var games = [ 
+	{
+		name : "Other_World",
+		platform : "Twine",
+		dir : "Other_World",
+		authors : [ "Nailea Llamas" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>Here this game is meant to take you into a state of mind. One that you are either taken into, or willingly experienced. Your decisions and interactions with the others around you decide how you effect them, and yourself. All those who have entered this state.</p>"
+	},{
+		name : "The Conversation",
+		platform : "Twine",
+		dir : "The Conversation",
+		authors : [ "Joseph Squires" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>You are a hiker who unwittingly wanders into a dragon's lair, and must have a conversation with it in the hopes of convincing it to let you go. But should you do or say things that further upset the dragon, it will kill you instead. There are 8 total endings, some of them hidden.</p>"
+	},{
+		name : "Ed G. Dating",
+		platform : "Twine",
+		dir : "Ed G. Dating",
+		authors : [ "Vivian Nguyen" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>The player's role in the game is the main character who is doing various tasks in order to get ready for their date.</p>"
+	},{
+		name : "Prison Break",
+		platform : "Twine",
+		dir : "Prison Break",
+		authors : [ "Vincet Titterton" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>Prison break takes place in a high security prison. The main character was in the wrong place at the wrong time, and has been falsely convicted of some unstated crime. Life in the prison consists of the same few activities repeated every day, along with a lot of time spent waiting around, doing nothing. The only way to break this endless cycle is to escape the prison.</p>"
+	},{
+		name : "Roguelike",
+		platform : "Twine",
+		dir : "Roguelike",
+		authors : [ "Milo Fisher" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>You are an adventurer exploring a mysterious tomb, riddled with traps, puzzles, and monsters. You can choose a class to provide you with unique bonuses as you traverse the tomb.</p>"
+	},{
+		name : "Somnograph",
+		platform : "Twine",
+		dir : "Somnograph",
+		authors : [ "Willow Tschudy" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>Somnograph is a short story about trying to wake up someone you know by looking through their memories. It uses and interconnected web and adaptive text and endings to change your history with the person, and add new contexts to the memories.</p>"
+	},{
+		name : "Secret Life of Eleanora Mitty",
+		platform : "Twine",
+		dir : "Secret Life of Eleanora Mitty",
+		authors : [ "AJ Blackadar" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>You slept through your alarm and need to get ready for work, but something is wrong. A game about daydreaming against your better judgement. Five endings.</p>"
+	},{
+		name : "Concrete",
+		platform : "Twine",
+		dir : "Concrete",
+		authors : [ "Mishant Suria" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>Concrete is a game that is an interactive fiction game about a lucid dream, where you explore a lifeless New York City. You explore several locations, and meet a few minor characters, but for the most part, you are alone in your own mind.</p>"
+	},{
+		name : "Kingdom of Jadesi",
+		platform : "Twine",
+		dir : "Kingdom of Jadesi",
+		authors : [ "Ziad Kedkad" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>Kingdom of Jadesi is a kingdom simulator semi-realistic test idea to condition future kings and leaders. You must carry your kingdom to become a global power but each decision you make has great benefits and consequences to you and your kingdom. Will you succeed in becoming a beloved leader and global super power?</p>"
+	},{
+		name : "Overseer",
+		platform : "Twine",
+		dir : "Overseer",
+		authors : [ "Waylon Peng" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>You are a space station AI. There's been a catastrophe. Wake up, there's work to do.</p>"
+	},{
+		name : "Broadborough: Paper City",
+		platform : "Twine",
+		dir : "Broadborough",
+		authors : [ "Aidan Barcia-Bacon" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>In Broadborough: Paper City, you play as a mayor trying to manage a city on the verge of ruin. Spend your weeks passing legislation, setting the budget, and listening to your advisors. If you can keep people happy and avoid all the imminent crises for 35 weeks, you win. Good luck!</p>"
+	},{
+		name : "Land of the Poorly Drawn",
+		platform : "Twine",
+		dir : "Land of the Poorly Drawn",
+		authors : [ "Nathan Grinnell" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>You're bored and decide to read one of the books your parents have told you to stay away from. You are transported into a world where everything is extremely poorly drawn. Your goal is to escape from this awful world so that you don't have to look at it ever again. Along the way, you'll meet plenty of wacky and poorly drawn characters.</p>"
+	},{
+		name : "Frank the Forager",
+		platform : "Twine",
+		dir : "Frank the Forager",
+		authors : [ "Laura Nakamura" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>Frank the Forager is a life dating sim about Frank and his obsession with foraging mushrooms. But, will foraging them be his final goal? Or will a new passion arise?</p>"
+	},{
+		name : "College Alienation",
+		platform : "Twine",
+		dir : "College Alienation",
+		authors : [ "Ian Richardson" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>College Alienation is about an alien agent who is sent to a community college on Earth and is tasked with collecting up to three items crucial to better understanding human society. On campus, you must explore and investigate the quest items without attracting suspicion. After you successfully gather as many items as you can, you will be evaluated on your total collection as well as how well you blended in.</p>"
+	},{
+		name : "The Hum",
+		platform : "Twine",
+		dir : "The Hum",
+		authors : [ "Alex Soliz" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>In a dystopian future, you have collected enough money to move to the colony on Mars. On your way to your new beginning, you wake up from CryoSleep early. Unsure of what's happening, you must decide your reality and escape. </p>"
+	},{
+		name : "Bond of Bards",
+		platform : "Construct",
+		dir : "Bond of Bards",
+		authors : [ "Nicholas Lau", " Madeleine Graboske", " Ethan Case" ],
+		showPlayButton : false,
+		showSourceDownload : true,
+		description : "<p>Bond of Bards is a short, musical, friendship-building game about forming a crew of singing pirates to serenade a vengeful kraken.</p>"
+	},{
+		name : "Amorphous",
+		platform : "Construct",
+		dir : "Amorphous",
+		authors : [ "Niklas Thomas", " Dylan Evenfield", " Jagdish Bal" ],
+		showPlayButton : false,
+		showSourceDownload : true,
+		description : "<p>Amorphous is the story of a formless blob looking for meaning. The blob escapes into its fantasies where it imagines what life could be like without depression, anxiety, and hollowness. This game is a platformer where each level represents an emotion. In each level, an entirely unique character with unique strengths, weaknesses, and abilities provides different gameplay styles every level. Explore what these characters represent to the blob, and how it chooses to cope with its suffering.</p>"
+	},{
+		name : "The Collector",
+		platform : "Construct",
+		dir : "The Collector",
+		authors : [ "Angela Wang", " Jan Ning" ],
+		showPlayButton : false,
+		showSourceDownload : true,
+		description : "<p>The Collector is a 2D movement-based platformer with only 1 level. The player starts off with little information of their movement controls other than the basic up/down left/right. As the player progresses, they begin to learn more skills which can now be used upon when the player resets. Chimes are a core mechanic in the game as it can be used as a powerup or as health. The game ends once the player runs out of chimes so management of resources is also a core aspect of the game.</p>"
+	},{
+		name : "Chase Your Dreams",
+		platform : "Construct",
+		dir : "Chase Your Dreams",
+		authors : [ "Kevin Lewis", " Joesph Squires", " Cameron Beattie" ],
+		showPlayButton : false,
+		showSourceDownload : true,
+		description : "<p>A game about chasing your dreams, float up further into the sky as you do, but avoid running into nightmares. You'll have to choose if the pain gets too high to continue if you hit one.</p>"
+	}
+];
+
+var authors = [ 
+	{
+		name : "Aidan Barcia-Bacon"
+	},{
+		name : "AJ Blackadar"
+	},{
+		name : "Alex Soliz"
+	},{
+		name : "Angela Wang"
+	},{
+		name : "Cameron Beattie"
+	},{
+		name : "Dylan Evenfield"
+	},{
+		name : "Ethan Case"
+	},{
+		name : "Ian Richardson"
+	},{
+		name : "Jagdish Bal"
+	},{
+		name : "Jan Ning"
+	},{
+		name : "Joesph Squires"
+	},{
+		name : "Kevin Lewis"
+	},{
+		name : "Laura Nakamura"
+	},{
+		name : "Madeleine Graboske"
+	},{
+		name : "Milo Fisher"
+	},{
+		name : "Mishant Suria"
+	},{
+		name : "Nailea Llamas"
+	},{
+		name : "Nathan Grinnell"
+	},{
+		name : "Nicholas Lau"
+	},{
+		name : "Niklas Thomas"
+	},{
+		name : "Vincet Titterton"
+	},{
+		name : "Vivian Nguyen"
+	},{
+		name : "Waylon Peng"
+	},{
+		name : "Willow Tschudy"
+	},{
+		name : "Ziad Kedkad"
+	}
+];
---
+++ data_winners.js
@@ -0,0 +1,157 @@
+
+var games = [ 
+	{
+		name : "Radio Fall",
+		platform : "Twine",
+		dir : "RadioFall",
+		authors : [ "Michelle Lytle" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>A game about communication and loneliness.</p>"
+	},{
+		name : "In an Orderly Fashion",
+		platform : "Twine",
+		dir : "InAnOrderlyFashion",
+		authors : [ "Oliver Berggren" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>The  border-crossing experience from Mexico to America.</p>"
+	},{	
+		name : "The Perfect Gift",
+		platform : "Twine",
+		dir : "ProjectLove",
+		authors : [ "Sam Feng" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>The perfect gift is a comedy visual novel about a boy trying to get a gift for the girl he fell in love with.</p>"
+	},{
+		name : "A Long Way Home",
+		platform : "Bitsy",
+		dir : "ALongWayHome",
+		authors : [ "Alicia Zhen", "Brandon Kharaka", "Joann Marie Wong" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>Our game is about a lost cat who must find their way home. The story is that you’re an outdoor can who was let out of the house to roam one evening. Upon leaving the house, the cat finds a butterfly and starts chasing it all around the neighborhood. Eventually, the cat loses its way home and must find a way back.</p>"
+	},{
+		name : "Hop Hop Donut",
+		platform : "Godot",
+		dir : "HopHopDonut",
+		authors : [ "Sam Feng", "Jose Salas" ],
+		showPlayButton : true,
+		showSourceDownload : false,
+		description : "<p>Donut the corgi had his owner kidnapped by an evil band of cats who envy the peace between humans and dogs, but luckily they aren't too bright. So, strap on your balloons with Donut and take to the skies, battling enemy critters in search of your beloved owner and find who is behind this nasty plot.</p>"
+	},{
+		name : "Calming Pressures",
+		platform : "Bitsy",
+		dir : "CalmingPressures",
+		authors : [ "Niccolo Abate", "Tate Pieper" ],
+		showPlayButton : true,
+		showSourceDownload : true,
+		description : "<p>Calming Pressures is an atmospheric story game about a young, ambitious fish, excited to find his dreams in the endless oceans. But when he ventures out and explores his world, he finds it to be small and caged in glass. He is in a fishtank.</p>"
+	}
+];
+
+var authors = [ 
+	{
+		name : "Alicia Zhen",
+		itch : "https://sad-boi-hours.itch.io/",
+		twitter : "yukidesoo",
+		instagram : "wallflower.png & @yukidesoo",
+		linkedin : "https://www.linkedin.com/in/alicia-zhen-466b8119b/",
+		email : "zhenalicia@gmail.com"
+	},{
+		name : "Ashley Lu"
+	},{
+		name : "Brandon Kharaka"
+	},{
+		name : "Carl Erez"
+	},{
+		name : "Changlin Yang"
+	},{
+		name : "Colin Watterson",
+		itch : "https://colinw.itch.io/",
+		linkedin : "https://www.linkedin.com/in/colin-watterson-63a4391a1/",
+		email : "ctwatter@ucsc.edu"
+	},{
+		name : "Enrico Widodo"
+	},{
+		name : "Fernanda Becerra",
+		instagram : "fernanda.b13",
+		email : "mfbecerr@ucsc.edu"
+	},{
+		name : "Harrison Kurtz"
+	},{
+		name : "Isaac Tremble-Pederson"
+	},{
+		name : "Isabelle Wong"
+	},{
+		name : "Ivan Martinez-Arias",
+		linkedin : "https://www.linkedin.com/in/ivan-martinez-arias-2898a0192/",
+		email : "martinez14ivan0427@gmail.com"
+	},{
+		name : "Jameson Danning",
+		website : "https://jdanning.myportfolio.com/",
+		itch : "https://jamesondanning.itch.io/",
+		instagram : "bigboijamo",
+		linkedin : "https://www.linkedin.com/in/jameson-danning-418440193/",
+		email : "jdanning@ucsc.edu"
+	},{
+		name : "Jess Wake"
+	},{
+		name : "Joann Marie Wong"
+	},{
+		name : "Jose Salas"
+	},{
+		name : "Justin Chang"
+	},{
+		name : "Kenneth Blum"
+	},{
+		name : "Lachlan Diaz"
+	},{
+		name : "Lucio Espinoza"
+	},{
+		name : "Maria Becerra"
+	},{
+		name : "Mariel Folkerts"
+	},{
+		name : "Mark Medved"
+	},{
+		name : "Matthew Boisvert",
+		linkedin : "https://www.linkedin.com/in/matthew-boisvert-aa5732158/",
+		email : "mboisver@ucsc.edu"
+	},{
+		name : "Matthew Boisvert"
+	},{
+		name : "Meichi Chen",
+		email : "mchen99@ucsc.edu"
+	},{
+		name : "Michelle Lytle",
+		email : "mnlytle@ucsc.edu"
+	},{
+		name : "Mingun Cho"
+	},{
+		name : "Nathan Ma"
+	},{
+		name : "Nathann Latimore",
+		twitter : "Sonar_101",
+		instagram : "sonar101",
+		youtube : "https://www.youtube.com/channel/UCLAU14RVIQezSmvyAwXZRxA",
+		email : "N.Latimore1013@gmail.com"
+	},{
+		name : "Niccolo Abate"
+	},{
+		name : "Oliver Berggren"
+	},{
+		name : "Sam Feng"
+	},{
+		name : "Tate Pieper"
+	},{
+		name : "Zhifeng Lu"
+	},{
+		name : "Sam Nguyen"
+	},{
+		name : "Victor Chung"
+	},{
+		name : "Tim D'Amore"
+	}
+];
